b51e7127397c3b7c16aa8b447a02ba10c36566c0
devmpedroso <devmatheuspedroso@gmail.com>
2024-10-24 23:06:35 -0300

    Layout home e calendar editados e basicamente prontos. Faltando detalhes no calendar (add linha separadora)

--- server.js
+++
@@ -1,7 +0,0 @@
-import "dotenv/config"; //tem que ser exportado na parte mais externa da aplicação, por isso está aqui
-import app from './src/app.js';
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log('Escutando na porta 3000');
-});
